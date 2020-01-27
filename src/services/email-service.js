3d5502a279f22f82e61e5357b63d7e5400f7e3b3
Natanael730i <natanaelmuller730@gmail.com>
2020-01-27 11:23:40 -0300

    Services e Validations

---
+++ src/services/email-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let config = require('../config');
+let sendgrid = require('sendgrid')(config.sendgridKey);
+
+exports.send = async (to, subject, body) => {
+    sendgrid.send({
+        to: to,
+        from: 'natanaelmuller730@gmail.com',
+        subject: subject,
+        html: body
+    });
+}
